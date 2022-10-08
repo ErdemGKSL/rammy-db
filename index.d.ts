f4f6251cadf5dec0acf9080801ef485db38033b4
Erdem <74366943+ErdemGKSL@users.noreply.github.com>
2022-10-08 12:49:48 +0300

    First

---
+++ index.d.ts
@@ -0,0 +1,13 @@
+export class RamDB<Model> {
+
+  data: Model;
+
+  constructor(ctx: { path: string, timeout: number, default: Model, customClasses: CustomClass<any>[] });
+
+  saveData(): Promise<void>;
+
+}
+
+interface CustomClass<T> {}
+
+export function toCustomClass<T>(mClass: T): CustomClass<T>;
---
+++ index.js
@@ -0,0 +1,39 @@
+const _ = require("lodash");
+const fs = require("fs");
+
+module.exports.RamDB = class RamDB {
+
+  constructor(args = {}) {
+    this.path = args.path;
+    this.customClasses = Object.fromEntries(args.customClasses.map(cc => [cc.constructor?.name, cc]));
+    this.data = args.default ?? [];
+    try {
+      this.data = JSON.parse(fs.readFileSync(this.path, "utf-8"), (k, v) => {
+        if (v?.customClass && this.customClasses?.[v.customClass]) return new (this.customClasses[v.customClass])(v.data);
+        return v;
+      });
+    } catch (err) { console.error(err) };
+    this.saveData = _.throttle(() => this.#saveData(), args.timeout || 5000);
+    this.saveData();
+    console.log({ d: this.data });
+  }
+
+  #saveData() {
+    let data = JSON.stringify(this.data, null, 2);
+    fs.writeFileSync(this.path, data);
+  }
+
+};
+
+module.exports.toCustomClass = function toCustomClass(mClass) {
+  if (mClass.prototype.__toJSON) return;
+  mClass.prototype.__toJSON = mClass.prototype.toJSON;
+  mClass.prototype.toJSON = function toJSON() {
+    const self = this;
+    return {
+      customClass: self.constructor.name,
+      data: self.__toJSON(),
+    }
+  };
+  return mClass;
+}
